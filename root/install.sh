d7af93da95ae72fdb40856237d2449a8c4da47e9
kiowadriver <mitch.riley1772@gmail.com>
2020-04-24 17:42:30 -0500

    fixed error in search string

--- root/install.sh
+++ root/install.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 # Change PHP_FPM to not clear env variables
-sed -i 's/;clear_env = yes/clear_env = no/' /etc/php/7.2/fpm/pool.d/www.conf
+sed -i 's/;clear_env = no/clear_env = no/' /etc/php/7.2/fpm/pool.d/www.conf
 
