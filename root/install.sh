a037ddcedfc7065e3a10978df31404838d7f25b5
kiowadriver <mitch.riley1772@gmail.com>
2020-04-24 23:35:35 -0500

    dynamic php-fpm
    install with most recent php-fpm

--- root/install.sh
+++ root/install.sh
@@ -1,5 +1,18 @@
 #! /bin/bash
 
-# Change PHP_FPM to not clear env variables
-sed -i 's/;clear_env = no/clear_env = no/' /etc/php/7.4/fpm/pool.d/www.conf
+# determine php path
+php_path=$(find /etc \( -iname "www.conf" \))
 
+# Change PHP_FPM to not clear env variables
+sed -i '/clear_env/c\clear_env = no/' $php_path
+
+php_bin=$(find /usr/sbin/ -print | grep -i fpm)
+
+# update supervisor script
+sed -i "s+command = /usr/sbin/php-fpm.*+command = $php_bin -R+" /etc/supervisor/conf.d/supervisord.conf
+
+# update nginx configuration, get the name of the .sock
+php_sock=$(sed -n -e 's/^.*listen = //p' $php_path)
+
+# replace the name in the nginx configuration file
+sed -i "s+.*fastcgi_pass unix:/run/php.*+                fastcgi_pass unix:$php_sock;+" /etc/nginx/sites-enabled/default
